6c6f610ce21c4375388a09ad0097c700512d007f
inseong <twin2688@naver.com>
2022-10-29 15:36:33 +0900

    22.10.29 ) 나머지 구하기.js

--- coding test/나머지 구하기.js
+++ coding test/나머지 구하기.js
@@ -1,3 +1,5 @@
-﻿function solution(num1, num2) {
+﻿// 프로그래머스 0단계 나머지 구하기 답 풀이
+// https://school.programmers.co.kr/learn/courses/30/lessons/120810
+function solution(num1, num2) {
    return num1 % num2 
 }
