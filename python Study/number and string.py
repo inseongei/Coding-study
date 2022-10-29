abc41c4d169f6296dd1146d6e13faf2c626201a4
inseong <twin2688@naver.com>
2022-10-29 14:56:08 +0900

    문자와 숫자열

---
+++ python Study/number and string.py
@@ -0,0 +1,21 @@
+﻿my_name = 'Python' #문자열 (사람을 위한 텍스트)
+my_age = 25 #숫자
+
+print(my_name, '은 이제',my_age,'살')
+
+my_next_age = 26
+
+print('내년에는', my_next_age, '살')
+
+multiply = 9*9 #=81
+divide = 30/5 #=6
+power = 2**10 #=1024
+reminder = 15%4 #=3
+
+print(multiply,divide,power,reminder)
+
+text = '2015' + '1991'
+number = 2015 + 1999
+
+print(text) #20151991
+print(number) # 4014
