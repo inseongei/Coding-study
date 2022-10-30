1e12567e406e041b9dfceb923cabc0cb37f6deff
inseong <twin2688@naver.com>
2022-10-30 20:44:33 +0900

    문제 풀이

---
+++ coding test/레벨 0/두수의 곱.js
@@ -0,0 +1,7 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120804
+// 프로그래머스 0단계 : 두 수의 곱 답 풀이
+function solution(num1, num2) {
+    return num1 * num2
+}
+
+// 두수의 곱은 주어진 매개변수 num1 * num2
