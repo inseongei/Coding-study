0e9c3713074f81f283cdd516de753e5c843bdbec
inseong <twin2688@naver.com>
2022-10-30 11:01:16 +0900

    두 수의 합 풀이

---
+++ coding test/두 수의 합.js
@@ -0,0 +1,7 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120802
+// 프로그래머스 0단계 : 두 수의 합
+function solution(num1, num2) {
+    return num1 + num2
+}
+
+// 말 그대로 .. num1과 num2를 더해주면 되는 문제
