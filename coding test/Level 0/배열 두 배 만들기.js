6b30c548b90590e34bbf505b1f163c513bad6b36
inseong <twin2688@naver.com>
2022-11-01 21:46:22 +0900

    문제 풀이

---
+++ coding test/Level 0/배열 두 배 만들기.js
@@ -0,0 +1,15 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120809
+// 프로그래머스 0단계 : 배열 두 배 만들기
+function solution(numbers) {
+    let result = []
+    for(let i = 0 ; i<numbers.length;i++){
+    result.push(numbers[i] * 2) 
+    }
+    return result
+}
+/*
+문제에서 주어진 배열의 요소를 2배한 result 라는 배열을 리턴하면되는 문제다
+1. for문을 통해서 해당 매개변수 numbers 배열 길이만큼 돌려주고
+result라는 새로 만든 배열에 push를 사용해서 * 2 를 해주고  result 배열에 넣어준다 
+2. 반복문이 끝나면 result를 리턴해주면 되는것
+*/
---
+++ coding test/Level 0/짝수의 합.js
@@ -0,0 +1,19 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120831
+// 프로그래머스 0단계 : 짝수의 합
+function solution(n) {
+    let sum = 0
+    for(let i = 2; i<=n;i++){
+        if(i % 2 === 0){
+            sum += i
+        }
+    }
+    return sum
+}
+
+/*
+1. 짝수의 합을 저장할 변수를 하나 만든다
+2. 반복문을 통해서 첫 시작점을 2 , 끝을 매개변수n으로 두고 i++
+ex ) n = 4 라면 for문에 1는 2,3,4
+3. 이렇게 반복 시킨 i에 짝수만 뽑아서 (i % 2 === 0)
+4. sum += i 하고  return sum 해주면 끝
+*/
