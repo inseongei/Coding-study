4016cebc5b6ce5613f6bdaa0680d84ab7881c07b
inseong <twin2688@naver.com>
2022-11-02 11:32:30 +0900

    문제 풀이

---
+++ coding test/Level 0/배열의 평균값.js
@@ -0,0 +1,16 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120817
+// 프로그래머스 0단계 : 배열의 평균값
+function solution(numbers) {
+    let sum = 0;
+   for(let i = 0; i< numbers.length;i++){
+       sum += numbers[i]
+   }
+    return sum / numbers.length
+}
+
+/*
+문제에서 주어진 정수 배열 numbers가 매개변수로 주어진다
+우리는 이 매개변수의 평균값을 구하면된다
+1. 반복문을 통해서 각 위치에 있는 변수들의 합계를 구하기 위해 sum이라는 변수를 만들고 반복문을 통해 합계를 구한다
+2. 그 합계와 numbers의 길이 만큼 나누어 주면 해당 매개변수의 평균값을 구할 수 있다 
+*/
---
+++ coding test/Level 0/중복된 숫자 개수.js
@@ -0,0 +1,18 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120583
+// 프로그래머스 0단계 : 중복된 숫자 개수
+function solution(array, n) {
+    let sum = 0 
+    for(let i = 0; i< array.length;i++){
+        if(n === array[i]){
+            sum++
+        }
+    }
+    return sum;
+}
+
+/*
+array라는 배열에 n이 몇개가 있는지 알아내는 문제이다
+나의 풀이법
+1. array의 길이만큼 반복을 한다음 조건문을 통해서 만약 n이 array[i] 랑 같다면 sum라는 변수에 1을 더해 주세요
+2. 그렇게 된다면 해당 배열의 n이 몇개가 같은지를 알게 되며 sum 리턴하면 문제를 풀 수 있다
+*/
