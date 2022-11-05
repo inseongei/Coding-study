4a80af248397f6a772ba87f889918ed10fa4e34f
inseong <twin2688@naver.com>
2022-11-05 12:26:54 +0900

    문제 풀이

---
+++ coding test/Level 0/문자열 뒤집기.js
@@ -0,0 +1,11 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120822
+// 프로그래머스 0단계 : 문자열 뒤집기
+function solution(my_string) {
+    return my_string.split('').reverse().join('')
+}
+/*
+이 문제는 메소드 split , reverse , join 메소드를 알고 있으면 금방 해결하는 문제는
+split : 문자열을 ('')을 기준으로 하나하나 쪼개서 배열로 만들어준다 (배열로 만들어주는 이유 : reverse라는 메소드는 배열 메소드 이기때문에)
+reverse : 배열을 뒤집어 주는 것 ex ) ['a','p','p','l','e'] => ['e','l','p','p','a']
+join : 배열을 풀어주는 메소드 이다 join('')을 하게 되면 배열을 다 풀고 문자열로 다시 만들어주어서 문제가 원하는 리턴값을 뽑아낼 수 있다
+*/
---
+++ coding test/Level 0/최대값 만들기(1).js
@@ -0,0 +1,13 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120847
+// 프로그래머스 0단계 : 최댓값 만들기(1)
+function solution(numbers) {
+     numbers.sort((a,b)=>a-b)
+    return numbers[numbers.length-1] * numbers[numbers.length-2]
+}
+
+/*
+주어진 배열에 최대값 2개를 곱해서 제일 큰 수를 만들어 내면 된다
+그렇기 때문에 
+1. 숫자가 섞여 있는 배열에서 오름차순 정렬 (sort)를 해준다
+2. 정렬이 된 배열에서 마지막 수와 그 마지막 수의 앞 숫자를 곱해주면 배열에서 제일 높은 값이 된다
+*/
