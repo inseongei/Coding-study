b44395622b6133d707f855b9c1853fe42e72124f
inseong <twin2688@naver.com>
2022-11-18 11:15:29 +0900

    문제 풀이

---
+++ coding test/Level 0/배열 회전시키기.js
@@ -0,0 +1,23 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120844
+// 프로그래머스 0단계 : 배열 회전시키기
+function solution(numbers, direction) {
+    let rig = [...numbers]
+    let lef = [...numbers]
+    
+    if(direction === "right"){
+		let last = rig.splice(numbers.length-1,1)    
+		rig.unshift(last[0])
+		return rig    
+    }else{
+		let first = lef.splice(0,1)
+		lef.push(first[0])
+		return lef
+    }
+}
+
+/* 문제풀이
+조건문과 배열 메소드 unshift , splice, push 를 사용하였다
+1. 조건문을 통해서 right 인지 left인지를 판별한뒤
+2. 하나의 변수 선언시키고 right면 마지막요소를 left면은 첫번째 요소를 잘라준다
+3. 그 자른 변수를 가지고 원본 numbers 배열에 right 면 뒤로 붙여주고 left 면 앞으로 붙여주면 끝 
+*/
---
+++ coding test/Level 0/최대값 만들기(2).js
@@ -0,0 +1,15 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120862
+// 프로그래머스 0단계 : 최대값 만들기 (2)
+function solution(numbers) {
+    numbers.sort((a,b)=>a-b)
+    let one = numbers[0] * numbers[1]
+    let two = numbers[numbers.length-1] * numbers[numbers.length-2]
+    return one > two ? one : two
+}
+
+/* 문제 풀이
+문제는 numbers 배열안에 요소들의 최대값을 구하는것이다
+1. sort 메소드를 사용해서 오름차순 정렬을 하였고
+2. 두개의 변수를 선언하여 하나는 앞에 두개 하나는 뒤에 두개를 곱해주었다 (앞에 두개를 곱하는 이유는 음수 * 음수 = 양수임을 대비해서 뒤에 두개를 곱할때보다 큰 경우가 있기 때문에)
+3. 삼항 연산자를 이용해서 간편하게 one > two 보다 크다면 one 리턴하고 그게 아니라면 two를 리턴하여 문제가 원하는 결과값을 뽑았다
+*/
