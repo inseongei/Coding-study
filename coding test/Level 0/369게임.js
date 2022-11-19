c894dab36ce5e209c7f4f3824ace5556c8482121
inseong <twin2688@naver.com>
2022-11-19 13:25:58 +0900

    문제 풀이

---
+++ coding test/Level 0/369게임.js
@@ -0,0 +1,20 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120891
+// 프로그래머스 0단계 : 369게임
+function solution(order) {
+    let arr = [...(order+'')]
+    let result = 0;
+    for(let i = 0 ; i<arr.length;i++){
+        if(Number(arr[i]) % 3 === 0 && Number(arr[i]) !== 0){
+           result++ 
+        }
+    }
+    return result
+}
+
+/* 문제풀이
+369 게임을 하고 order 라는 매개변수는 머쓱이 차례에 있는 숫자를 의미한다
+1. 우리는 order를 배열로 만들어준다음 각 숫자열 하나하나를 보면서 박수를 몇번 쳐야하는지 (result)를 알수 있어야 한다
+2. 반복문을 통해서 배열로 만든 길이만큼 반복을 시키고
+3. 조건문을 통해서 arr[i] 번째가 % 3 === 0 이면  369 중에 하나이니까 result(박수)를 ++ 하면된다
+4. 하지만 0일때의 조건도 걸어주어야 문제가 원하는 모든 테스트 케이스에 통과할 수 있다
+*/
---
+++ coding test/Level 0/중복된 문자 제거.js
@@ -0,0 +1,12 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120888
+// 프로그래머스 0단계 : 중복된 문자 제거
+function solution(my_string) {
+  return [...new Set(my_string)].join('')
+}
+/* 문제 풀이
+이문제는 매개변수로 주어진 my_string 이라는 문자열의 중복을 제거해주는 문제이다
+new Set이라는 메소드만 알고 있으면 쉽게 풀 수 있는 문제
+1. 중복값이 있는 my_string 배열로 만들고
+2. new Set을 활용해서 배열안에 중복을 제거해준다.
+3.join 이용해서 문제가 원하는 리턴값을 뽑아낼 수 있다 
+*/
