e234e9b9a1089141d3a1da7f7c1376711becd9ea
inseong <twin2688@naver.com>
2022-11-23 15:07:59 +0900

    문제 풀이

---
+++ coding test/Level 0/진료순서 정하기.js
@@ -0,0 +1,19 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120835
+// 프로그래머스 0단계 : 진료순서 정하기
+function solution(emergency) {
+    let array = [...emergency]
+    array.sort((a,b)=>b-a)
+    let result = [];
+    for(let i = 0; i<array.length;i++){
+        result.push(array.indexOf(emergency[i])+1)
+    }
+    return result
+}
+
+/* 문제 풀이
+응급퍼센트에 따라서 result(진료 순서)를 리턴하는 문제이다
+1. array 라는 새로운 배열에 스프레드 문법을 사용해서 복사를 해준다
+2. array를 내림차순으로 정렬을한다 (이말은 즉 응급률이 높은 순서대로 정렬을 한것)
+3. 반복문을 통해서 result 라는 새로운 배열에 array의 indexOf(emergency[i])를 찾아내주면 된다
+4. +1 을 해준 이유는 배열이기 때문에 0번째가 있기때문에 0번째는 즉 1번째 라는 뜻으로 +1을 해주고 리턴해주면 문제에서 원하는 리턴값을 뽑아낼 수 있다 
+*/
---
+++ coding test/Level 0/한 번만 등장한 문자.js
@@ -0,0 +1,14 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120896
+// 프로그래머스 0단계 : 한 번만 등장한 문자
+function solution(s) {
+    const dict = {}
+    // dict에 각 스펠링 별 등장 횟수 정리
+    s.split("").forEach((i)=> dict[i] = (dict[i] || 0) +1 )
+    return Object.entries(dict).map(a => a[1] === 1 ? a[0] : null).filter(a => a !== null).sort().join("")
+}
+
+/* 문제 풀이
+1. 매개변수 s를 split를 사용해서 배열로 만들어 준다음 forEach문을 사용해서 dict[i]요소에 같은 요소가 있으면 1를 더하는 식을 적었다
+2. object.entries를 사용하고 map을 돌리면서 a[1] (즉 a:3) 개수를 뜻하는 a[1]이 1이라면 우리는 그것을 빼오고 아니면 null로 처리한다
+3. filter를 사용해서 그 빼온 알파벳들을 오름차순 정렬을 하고 join('')으로 문자열 처리를 해주면 끝
+*/
