dcf1e4aa2da64a4feb1ab4c588d73a2fa4aa3e46
inseong <twin2688@naver.com>
2022-11-25 12:59:40 +0900

    문제 풀이

---
+++ coding test/Level 0/숨어있는 숫자의 덧셈(2).js
@@ -0,0 +1,15 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120864
+// 프로그래머스 0단계 : 숨어있는 숫자의 덧셈(2)
+function solution(my_string) {
+    const nums = my_string
+    .match(/[0-9]+/g)
+
+  return nums ?  nums.map(num => +num).reduce((a, c) => a + c, 0) : 0
+
+}
+
+/* 문제 풀이
+1. 정규식과 match를 사용해서 my_string문자열에 0~9까지의 숫자가 얼마나 있는지를 확인한다
+2.map을 통해서 전부 숫자열로 바꿔주고 reduce를 이용해서 더해준다
+3. 문제에서 문자열에 자연수가 없는 경우가 있으니 0을 리턴해주는 식까지 만들면 된다
+*/
---
+++ coding test/Level 0/잘라서 배열로 저장하기.js
@@ -0,0 +1,20 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120913
+// 프로그래머스 0단계 : 잘라서 배열로 저장하기
+function solution(my_str, n) {
+    let result = [];
+    let array = my_str.split('')
+    for(let i = 0 ; i<my_str.length;i++){
+        let one = array.splice(0,n)
+        one.length === 0 ? null : result.push(one.join(''))
+    }
+    return result
+}
+
+/* 문제 풀이
+splice와 slice라는 배열 자르기 메소드가 있는데 나는 원본 배열을 건드는 splice를 사용하였다
+1. result라는 빈배열과 array라는 my_str이라는 문자열을 배열로 만든 하나의 새로운 배열을 만들었다
+2. 반복문을 통해서 my_str의 길이 만큼 반복을 시켜주고
+3. one이라는 변수에 문자열을 배열로 만든 array의 splice를 하였다 0,n 만큼 그러면 반복을 하면서 array라는 배열에 0번째부터 n까지 잘려 나갈 것이고
+4. 삼항 연산자를 통해서 one의 길이가 0 이면 끝난거니까 null 그렇지 않으면 아까 만든 result에 push해준다 one.join('') 해준것을
+그러면 문제에서 원하는 리턴 값을 뽑아낼 수 있다
+*/
