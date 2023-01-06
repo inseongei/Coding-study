6461d57eb2047e69c81df9346fba46a8a24af42b
inseongei <twin2688@naver.com>
2023-01-06 13:31:44 +0900

    문제 풀이

---
+++ coding test/Level 0/저주의 숫자 3.js
@@ -0,0 +1,20 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120871
+// 프로그래머스 0단계 : 저주의 숫자 3
+function solution(n) {
+    let x3 = 0
+    // n까지 반복
+    for(let i = 1 ; i <= n ; i ++) {
+        // i에 증가에 따른 x3의 증가
+        x3++
+        // 3이 들어가지 않으며, 3의 배수 또한 아닌 경우까지 반복
+        while(x3.toString().includes('3') || x3%3 === 0) {
+            x3++
+        }
+    }
+    return x3
+}
+
+/* 문제 풀이 (kwb020312님꺼 참고)
+나는 while문의 조건을 하지 못해 실패하였다
+이문제는 x3을 계속 증가시키면서  x3.toString().includes('3') || x3%3 === 0 이 2가지의 조건에 걸리지 않으면 계속 증가시키면 문제가 원하는 답을 찾을 수 있는 문제이다
+*/
---
+++ coding test/Level 0/특이한 정렬.js
@@ -0,0 +1,14 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/120880
+// 프로그래머스 0단계 : 특이한 정렬
+function solution(numlist, n) {
+    return numlist.sort((a,b) => {
+        const [aGab, bGab] = [Math.abs(a-n), Math.abs(b-n)]
+        // 거리가 같다면 더 큰 수를 우선 배치
+        if(aGab === bGab) return b-a
+        // 다르다면 거리별 오름차순 정렬
+        return aGab-bGab
+    })
+}
+/* 문제 풀이 ( kwb020312님꺼 참고 )
+1. numlist를 정렬을 할때 거리가 같다면 큰 수를 우선 배치하게 하였다 Math.abs로 음수는 신경쓰지 않게 하였고 조건문을 사용해서 정렬을 함
+*/
