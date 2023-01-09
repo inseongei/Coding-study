64172dc571916ec7f601217273e49a11245c6989
inseongei <twin2688@naver.com>
2023-01-09 10:26:14 +0900

    문제 풀이

---
+++ coding test/Level 1/짝수와 홀수.js
@@ -0,0 +1,10 @@
+﻿// https://school.programmers.co.kr/learn/courses/30/lessons/12937
+// 프로그래머스 0단계 : 짝수와 홀수 
+
+function solution(num) {
+    return num % 2 === 0 ? "Even" : "Odd"
+}
+
+/* 문제 풀이
+문제에서 짝수와 홀수의 조건 num % 2를 사용해서 삼항 연산자를 이용해 Even과 Odd를 나타내었다 
+*/
